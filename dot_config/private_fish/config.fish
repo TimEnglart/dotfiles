69c49005a87a56993733db2eec85e433a3d00103
Tim Englart <timenglartdev@gmail.com>
2023-02-18 19:27:51 +1000

    update files

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -3,3 +3,8 @@
 #and not set -q TMUX
 #    exec tmux -u new -A -D -t f4h
 #end
+
+# Add our scripts to the path
+fish_add_path $HOME/.scripts/bin
+# Register the OS as an environment var, detected by the OS script 
+set -gx OS (os)
